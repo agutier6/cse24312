d800a6a8759a419cd0de23e6214c703452159197
Matthew Morrison <matt.morrison@nd.edu>
2020-06-03 14:19:22 -0400

    README.txt Push for Full Course

---
+++ 03-Part1/charHash.cpp
@@ -0,0 +1,61 @@
+/**********************************************
+* File: voidFunc.cpp
+* Author: Matthew Morrison
+* Email: matt.morrison@nd.edu
+* 
+* Example of allocating multiple version of data 
+* with different types of elements
+*
+* Shows void pointers as "Pass by reference"
+*
+* Lecture 02 - Part 2 - Slide 29
+**********************************************/
+
+#include <iostream>
+#include <cstdlib>
+
+#define COUT std::cout
+#define ENDL std::endl
+
+void hashFunc( int* reference, int input, int arraySize ){
+	
+	int bucket = (input % arraySize);
+	
+	*(reference + bucket) = input;
+	
+}
+
+
+void printHashValues( int* reference, long unsigned int arraySize ){
+	
+	for(long unsigned int iter = 0; iter < arraySize; iter++ ){
+		
+		COUT << "Bucket " << iter << " contains " << *(reference + iter) << ENDL;
+		
+	}
+	
+}
+
+int main(void){
+	
+	long unsigned int arraySize = 11;
+	
+	int* hashPtr = (int *) malloc( arraySize * sizeof(int) );
+	
+	// Hash seven values 
+	hashFunc( hashPtr, 2, arraySize );
+	hashFunc( hashPtr, 22, arraySize );
+	hashFunc( hashPtr, 18, arraySize );
+	hashFunc( hashPtr, 81, arraySize );
+	hashFunc( hashPtr, 69, arraySize );
+	hashFunc( hashPtr, 53, arraySize );
+	hashFunc( hashPtr, 109, arraySize );
+	
+	// Printing the values. Set the reference back to numbers
+	printHashValues( hashPtr, arraySize );
+	
+	// Free void* with allocated memory
+	free( hashPtr );
+
+	return 0;
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+These code segments follow the order for the CSE 24312 course at Notre Dame
+
+Each folder corresponds to a Lecture, and the order of explanation is shown in the slides
+
+The code is broken into several parts to show students the progression of code design. Several files may appear almost identical with small changes. This is by design.
+
+Follow the order of the slide set found at https://sites.google.com/a/nd.edu/morrison/courses/cse24312/lecture-notes/
