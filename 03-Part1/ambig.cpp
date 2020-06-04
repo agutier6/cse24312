a6293a15ab9c795ab145d683c1a09a3f4ecca7c3
Matthew Morrison <matt.morrison@nd.edu>
2020-06-04 15:33:19 -0400

    Lecture 03 - Part 1 - Update

--- 03-Part1/ambig.cpp
+++ 03-Part1/ambig.cpp
@@ -22,18 +22,6 @@ void PrintInt (int i) {
    std::cout << i << std::endl;
 }
 
-/********************************************
-* Function Name  : PrintMe
-* Pre-conditions : int& i
-* Post-conditions: none
-* 
-* Overloaded Function the Prints an integer 
-********************************************/
-void PrintInt (int& i) {
-   i = i + 2;
-   std::cout << i << std::endl;
-}
-
 
 /********************************************
 * Function Name  : PrintMe
@@ -42,23 +30,11 @@ void PrintInt (int& i) {
 * 
 * Overloaded Function the Prints an integer 
 ********************************************/
-
-/* DELIBERATE COMPILER ERROR 
-	/escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp: In function ‘void PrintInt(int)’:
-	/escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:53:6: error: redefinition of ‘void PrintInt(int)’
-	 void PrintInt (const int i) {
-		  ^
-	/escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:20:6: error: ‘void PrintInt(int)’ previously defined here
-	 void PrintInt (int i) { */
 void PrintInt (const int i) {
-	
-   /* Another deliberate compiler error */
-   /* const variables cannot be changed */
-   /* /escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:60:6: error: assignment of 
-      read-only parameter ‘i’
-	  i = i + 2; */
+
    i = i + 2;
    std::cout << i << std::endl;
+   
 }
 
 /********************************************
@@ -71,17 +47,6 @@ void PrintInt (const int i) {
 int main(){
 
 	int tempX = 25;
-
-	/* Since the overload call is ambiguous, this will produce a compiler error as well */
-	/* This is because it can't tell between PrintInt(int tempX) and PrintInt(int& tempX) 
-	   /escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp: In function ‘int main()’:
-       /escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:86:16: error: call of overload                ed ‘PrintInt(int&)’ is ambiguous 
-       PrintInt(tempX);
-	   /escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:86:16: note: candidates are:
-	   /escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:20:6: note: void PrintInt(int)
-	    void PrintInt (int i) {
-		/escnfs/home/mmorri22/cse20133/25-AdvCPPIntro/ambig.cpp:32:6: note: void PrintInt(int&)
-              void PrintInt (int& i) */
 
 	PrintInt(tempX);
 
