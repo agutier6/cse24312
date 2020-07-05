76d6609dd57d6d6c291b13c8e9d6bd15ff982251
Matthew Morrison <matt.morrison@nd.edu>
2020-07-05 07:44:52 -0400

    Lecture 18 - Part 1 Code

---
+++ 18-Part1/programs/testBST1.cpp
@@ -0,0 +1,13 @@
+#include "../classes/BST1.h"
+#include <iostream>
+
+#define COUT std::cout
+#define ENDL std::endl
+
+int main(){
+	
+	// Initialize the nodes
+	BST< char > theTree;
+	
+	return 0;
+}
